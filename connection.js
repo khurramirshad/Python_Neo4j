fd4b9e8b72cb24f50d75210a6fe977b57a019efa
khuram.irshad@gmail.com <khuram.irshad@gmail.com>
2024-07-14 21:47:37 +0500

    Defining module name

--- connection.js
+++ connection.js
@@ -1,26 +1,25 @@
-define(['neo4j-driver'], function(neo4j) {
-    // Replace with your Neo4j instance details
-    const URI = 'bolt://localhost:7687';
-    const USER = 'neo4j';
-    const PASSWORD = '12345678';
-    
-    const driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
+define('neo4jModule2', ['neo4j-driver'], function(neo4j) {
+  // Replace with your Neo4j instance details
+  const URI = 'bolt://localhost:7687';
+  const USER = 'neo4j';
+  const PASSWORD = '12345678';
   
-    async function connect() {
-      const session = driver.session();
-      console.log("Hello");
-      try {
-        const result = await session.run('MATCH (n) RETURN n LIMIT 1');
-        console.log(result.records);
-      } catch (error) {
-        console.error('Error connecting to Neo4j', error);
-      } finally {
-        await session.close();
-      }
-    }
-  
-    connect().then(() => driver.close());
-  });
+  const driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
 
+  async function connect() {
+    const session = driver.session();
+    console.log("Hello");
+    try {
+      const result = await session.run('MATCH (n) RETURN n LIMIT 1');
+      console.log(result.records);
+    } catch (error) {
+      console.error('Error connecting to Neo4j', error);
+    } finally {
+      await session.close();
+    }
+  }
+
+  connect().then(() => driver.close());
+});
 
   function submitQuery() {}
